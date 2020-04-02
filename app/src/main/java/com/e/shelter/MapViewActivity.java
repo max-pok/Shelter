bb29da3ecf85e7fdff03a507daa5b7704b99aeab
Max Pokidaylo <maxim.p9@gmail.com>
2020-04-02 21:54:24 +0300

    1. shelters json file -> mongoDB. 2. show shelters on map from mongoDB.
    Signed-off-by: Max Pokidaylo <maxim.p9@gmail.com>

--- app/src/main/java/com/e/shelter/MapViewActivity.java
+++ app/src/main/java/com/e/shelter/MapViewActivity.java
@@ -1,24 +1,36 @@
 package com.e.shelter;
 
 import androidx.fragment.app.FragmentActivity;
+import android.content.Context;
 import android.os.Bundle;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import android.os.StrictMode;
 
 public class MapViewActivity extends FragmentActivity implements OnMapReadyCallback {
-    GoogleMap mapAPI;
+    GoogleMap map;
     SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_window);
-
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
@@ -26,19 +38,68 @@ public class MapViewActivity extends FragmentActivity implements OnMapReadyCallb
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mapAPI = googleMap;
-        mapAPI.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.day_map));
-        LatLng latLng = new LatLng(31.067197, 35.034576);
-        mapAPI.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
-        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mapAPI.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16.0f));
+        map = googleMap;
+        map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.day_map));
+        // TODO: add current location.
+//        LatLng latLng = new LatLng(31.067197, 35.034576);
+//        map.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
+//        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16.0f));
+        map.getUiSettings().setMapToolbarEnabled(false);
+        add_shelters_into_map(map);
     }
 
-    public void add_shelters_into_map(GoogleMap googleMap) { }
+    public void add_shelters_into_map(GoogleMap googleMap) {
+        MongoClient mongoClient = new MongoClient("10.0.2.2", 27017);
+        DB shelter_db = mongoClient.getDB("SafeZone_DB");
+        DBCollection shelter_db_collection = shelter_db.getCollection("Shelters");
+        DBCursor cursor = shelter_db_collection.find();
+        while(cursor.hasNext()) {
+            BasicDBObject object = (BasicDBObject) cursor.next();
+            LatLng latLng = new LatLng(Double.parseDouble(object.getString("lat")), Double.parseDouble(object.getString("lon")));
+            googleMap.addMarker(new MarkerOptions().position(latLng).title(object.getString("name")));
+        }
+    }
 
-    public void add_shelters_to_mongodb() { }
+    public void add_shelters_to_mongodb() {
+        // TODO: add more information, for example: open/close, accessibility, capacity.
+        try {
+            JSONArray obj = new JSONArray(loadJSONFromAsset(getApplicationContext()));
+            MongoClient mongoClient = new MongoClient("10.0.2.2", 27017);
+            DB shelter_db = mongoClient.getDB("SafeZone_DB");
+            DBCollection shelter_db_collection = shelter_db.getCollection("Shelters");
+            for (int i = 0 ; i < obj.length() ; i++) {
+                JSONObject value = (JSONObject) obj.get(i);
+                BasicDBObject document = new BasicDBObject();
+                document.put("name", value.get("name"));
+                document.put("lat", value.get("lat"));
+                document.put("lon", value.get("lon"));
+                shelter_db_collection.insert(document);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void current_location(GoogleMap googleMap) { }
 
+    public void generateNoteOnSD(Context context, String sFileName, String sBody) {}
+
+    public String loadJSONFromAsset(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("shelters.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
+    }
 
 }
