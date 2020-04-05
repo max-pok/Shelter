a5de46f71db49cd7da0d32b10c4b85d14fafa608
Max Pokidaylo <maxim.p9@gmail.com>
2020-04-06 01:31:30 +0300

    added Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    triggers {
+        cron('H */8 * * *') //regular builds
+        pollSCM('* * * * *') //polling for changes, here once a minute
+    }
+
+    stages {
+        stage('Checkout') {
+            steps { //Checking out the repo
+                checkout changelog: true, poll: true, scm: [$class: 'GitSCM', branches: [[name: '*/map_view']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git', url: 'https://github.com/max-pok/Shelter.git']]]
+            }
+        }
+        stage('Unit & Integration Tests') {
+            steps {
+                script {
+                    try {
+                        sh './gradlew clean UnitTest --no-daemon' //run a gradle task
+                    } finally {
+                        junit '**/build/test-results/test/*.xml' //make the junit test results available in any case (success & failure)
+                    }
+                }
+            }
+        }
+        stage('Publish Artifact to Nexus') {
+            steps {
+                sh './gradlew publish --no-daemon'
+            }
+        }
+    }
+    post {
+        always { //Send an email to the person that broke the build
+            step([$class                  : 'Mailer',
+                  notifyEveryUnstableBuild: true,
+                  recipients              : [emailextrecipients([[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider']])].join(' ')])
+        }
+    }
+}
