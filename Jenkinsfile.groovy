e9b9f28aaaa116f4db820195a371f4b380d74ff0
Max Pokidaylo <maxim.p9@gmail.com>
2020-04-06 11:49:52 +0300

    update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,11 +15,8 @@ pipeline {
         stage('Unit & Integration Tests') {
             steps {
                 script {
-                    try {
-                        sh './gradlew clean test --no-daemon' //run a gradle task
-                    } finally {
                         junit '**/build/test-results/test/*.xml' //make the junit test results available in any case (success & failure)
-                    }
+
                 }
             }
         }
