8cb149ddc4348582c2a63c57360e04113b400c08
saritdi <45334085+saritdi@users.noreply.github.com>
2020-04-06 22:15:38 +0300

    PM2020T22-23

--- app/src/main/java/com/e/shelter/ContactPage.java
+++ app/src/main/java/com/e/shelter/ContactPage.java
@@ -10,5 +10,6 @@ public class ContactPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contacts_of_municipality);
+
     }
 }
