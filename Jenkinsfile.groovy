af52030189d6c4810c601097723adc96470f8c6d
adiratia <adir@ac.sce.ac.il>
2020-04-06 19:14:44 +0300

    PM2020T22-40 update 6/4

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,5 @@
 pipeline {
     agent any
-
-    docker {
-        image 'windsekirun/jenkins-android-docker:1.1.1'
-    }
     options {
         // Stop the build early in case of compile or test failures
         skipStagesAfterUnstable()
