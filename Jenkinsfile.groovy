43048593834060a173bd2ec1a05743ccac6884fd
Max Pokidaylo <maxim.p9@gmail.com>
2020-04-19 21:06:35 +0300

    jenkins file update 2 (send email)
    Signed-off-by: Max Pokidaylo <maxim.p9@gmail.com>

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -41,15 +41,9 @@ pipeline {
     }
     post {
         always {
-            emailext subject: '$DEFAULT_SUBJECT',
-                    body: '$DEFAULT_CONTENT',
-                    recipientProviders: [
-                            [$class: 'CulpritsRecipientProvider'],
-                            [$class: 'DevelopersRecipientProvider'],
-                            [$class: 'RequesterRecipientProvider']
-                    ],
-                    replyTo: '$DEFAULT_REPLYTO',
-                    to: '$DEFAULT_RECIPIENTS'
+            emailext body: 'Build Update',
+                    to: "maxim.p9@gmail.com",
+                    subject: 'Build failed in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER'
         }
     }
 }
