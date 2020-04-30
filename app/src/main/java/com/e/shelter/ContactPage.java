04ddb069849b844dc05b69d01b9efb0a0ad5be45
saritdi <45334085+saritdi@users.noreply.github.com>
2020-04-30 20:39:59 +0300

    PM2020T22-90
    PM2020T22-90

--- app/src/main/java/com/e/shelter/ContactPage.java
+++ app/src/main/java/com/e/shelter/ContactPage.java
@@ -1,10 +1,21 @@
 package com.e.shelter;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
-import com.e.shelter.R;
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class ContactPage extends AppCompatActivity {
 
@@ -12,5 +23,16 @@ public class ContactPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contacts_of_municipality);
+        createContactDataBase();
+
     }
+    public void createContactDataBase()
+    {
+        ContactPageThread contactThread= new ContactPageThread();
+        contactThread.start();
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/e/shelter/ContactPageThread.java
@@ -0,0 +1,65 @@
+package com.e.shelter;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+public class ContactPageThread extends Thread{
+    public static String name;
+    public static String phoneNumber;
+    //c'tor
+    ContactPageThread(){
+        this.name="";
+        this.phoneNumber="";
+    }
+    public void run()
+    {
+        try {
+
+            //Connect to MongoDB
+            MongoClient mongoClient = new MongoClient("10.0.2.2", 27017);
+            MongoDatabase database = mongoClient.getDatabase("SafeZone_DB");
+            MongoCollection<Document> contactCollection = database.getCollection("contactPage");
+            //Find if the email exist in users collection according to email
+
+            Document myDoc = contactCollection.find(eq("name", "עיריית באר שבע")).first();
+            if(myDoc!=null)
+            {
+                System.out.println("you have this database");
+            }
+            else {
+                //new Document for users collection
+                ArrayList<Document> newContact = new ArrayList<Document>();
+                newContact.add(new Document().append("name", "עיריית באר שבע").append("phoneNumber", "08-6463777/106"));
+                newContact.add(new Document().append("name", "משטרת ישראל").append("phoneNumber", "100"));
+                newContact.add(new Document().append("name", "מגן דוד אדום").append("phoneNumber", "101"));
+                newContact.add(new Document().append("name", "כיבוי אש").append("phoneNumber", "102"));
+                newContact.add(new Document().append("name", "פיקוד העורף").append("phoneNumber", "104"));
+                newContact.add(new Document().append("name", "בית החולים סורוקה").append("phoneNumber", "08-6400111"));
+                newContact.add(new Document().append("name", "חברת חשמל").append("phoneNumber", "103"));
+
+                //insert the document to users collection
+                contactCollection.insertMany(newContact);
+            }
+
+            //close the DB connection
+            mongoClient.close();
+        } catch (MongoException m) {
+            Log.e("Error " + m, "" + m);
+        }
+    }
+}
