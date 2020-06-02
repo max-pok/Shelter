d5315462199316115c79fe62c99e14f4a3da9abf
adiratia <adir@ac.sce.ac.il>
2020-06-02 12:43:37 +0300

    PM2020T22-12
    edit user unit tests

---
+++ app/src/main/java/com/e/shelter/validation/NameValidator.java
@@ -0,0 +1,21 @@
+package com.e.shelter.validation;
+
+public class NameValidator {
+
+
+    public static boolean isValidName(String name){
+
+            if(name == null || name.isEmpty()) return false;
+
+            StringBuilder sb = new StringBuilder();
+            boolean found = false;
+            for(char c : name.toCharArray()){
+                if(Character.isDigit(c)){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+}
---
+++ app/src/main/java/com/e/shelter/validation/PhoneValidator.java
@@ -0,0 +1,20 @@
+package com.e.shelter.validation;
+
+public class PhoneValidator {
+
+    public static boolean isValidPhone(String phone){
+
+        if(phone == null || phone.isEmpty()) return false;
+
+        StringBuilder sb = new StringBuilder();
+        boolean found = false;
+        for(char c : phone.toCharArray()){
+            if(Character.isAlphabetic(c)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
